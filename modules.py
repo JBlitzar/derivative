bf1936a3a34df5e58fc46a7bcd70e268bce51818
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-09-22 12:28:01 -0700

    simplify

--- modules.py
+++ modules.py
@@ -1,4 +1,6 @@
 from typing import Type
+import math
+
 class Expression:
     def __init__(self) -> None:
         pass
@@ -9,6 +11,9 @@ class Expression:
     def derivative(self):
         return Expression()
     
+    def simplify(self):
+        return self
+    
     def __repr__(self):
         return "Expression"
     
@@ -16,9 +21,6 @@ class Expression:
 class X(Expression):
     def __init__(self) -> None:
         super().__init__()
-
-
-
 
     def __call__(self, x: float) -> float:
         return x
@@ -29,11 +31,13 @@ class X(Expression):
     
     def __repr__(self):
         return "X"
+    
+    def simplify(self):
+        return self
 
 class Constant(Expression):
     def __init__(self, k: float) -> None:
         super().__init__()
-
 
         self.k = k
 
@@ -45,7 +49,24 @@ class Constant(Expression):
         return Constant(0.0)
     
     def __repr__(self):
-        return self.k.__repr__()
+        return f"{self.k.__repr__()}"
+    
+    def simplify(self):
+        if isinstance(self.k, Constant):
+            return Constant(self.k.k)
+        return self
+    
+    def __add__(self, other):
+        return Constant(self.k + other.k)
+    
+    def __sub__(self, other):
+        return Constant(self.k - other.k)
+    
+    def __mul__(self, other):
+        return Constant(self.k * other.k)
+    
+    def __div__(self, other):
+        return Constant(self.k * other.k)
     
 class MultipliedBy(Expression):
     def __init__(self, a,b) -> None:
@@ -64,6 +85,15 @@ class MultipliedBy(Expression):
     
     def __repr__(self):
         return f"({self.a.__repr__()} * {self.b.__repr__()})"
+    
+    def simplify(self):
+        if isinstance(self.a, Constant) and isinstance(self.b, Constant):
+            return Constant(self.a * self.b).simplify()
+        
+        if isinstance(self.a, Constant) and self.a.k == 0.0 or isinstance(self.b, Constant) and self.b.k == 0.0:
+            return Constant(0.0)
+
+        return MultipliedBy(self.a.simplify(), self.b.simplify())
 
 class DividedBy(Expression):
     def __init__(self, a,b) -> None:
@@ -82,6 +112,14 @@ class DividedBy(Expression):
     
     def __repr__(self):
         return f"({self.a.__repr__()} / {self.b.__repr__()})"
+    
+    def simplify(self):
+        self.a = self.a.simplify()
+        self.b = self.b.simplify()
+        if isinstance(self.a, Constant) and isinstance(self.b, Constant):
+            return Constant(self.a / self.b).simplify()
+
+        return DividedBy(self.a.simplify(), self.b.simplify())
 
 class Add(Expression):
     def __init__(self, a,b) -> None:
@@ -100,6 +138,14 @@ class Add(Expression):
     
     def __repr__(self):
         return f"({self.a.__repr__()} + {self.b.__repr__()})"
+    
+    def simplify(self):
+        self.a = self.a.simplify()
+        self.b = self.b.simplify()
+        if isinstance(self.a, Constant) and isinstance(self.b, Constant):
+            return Constant(self.a + self.b).simplify()
+
+        return Add(self.a.simplify(), self.b.simplify())
 
 class Subtract(Expression):
     def __init__(self, a,b) -> None:
@@ -118,6 +164,15 @@ class Subtract(Expression):
     
     def __repr__(self):
         return f"({self.a.__repr__()} - {self.b.__repr__()})"
+    
+    def simplify(self):
+        self.a = self.a.simplify()
+        self.b = self.b.simplify()
+        if isinstance(self.a, Constant) and isinstance(self.b, Constant):
+            
+            return Constant(self.a - self.b).simplify()
+
+        return Subtract(self.a, self.b)
 
 class PolynomialExponent(Expression):
     def __init__(self, exponent: float, baseExpression: Expression = X) -> None:
@@ -138,10 +193,38 @@ class PolynomialExponent(Expression):
         return f"({self.baseExpression.__repr__()} ^ {self.exponent.__repr__()})"
     
 
+    def simplify(self):
+        self.baseExpression = self.baseExpression.simplify()
+        if isinstance(self.baseExpression, Constant):
+            return Constant(self.baseExpression ** self.exponent).simplify()
+
+        return PolynomialExponent(self.exponent, self.baseExpression.simplify())
+    
+class EToTheF(Expression):
+    def __init__(self, f: Expression = X) -> None:
+        super().__init__()
+
+        self.f = f
+
+    def __call__(self, x: float) -> float:
+        return math.e ** self.f(x)
+    
+    
+    def derivative(self) -> Type[Expression]:
+        return MultipliedBy(self.f, self)
+    
+    def __repr__(self):
+        return "e^X"
+    
+    def simplify(self):
+        if isinstance(self.f, Constant) :
+            return Constant(math.e ** self.exponent).simplify()
+
+        return EToTheF(self.f.simplify())
 
 
 if __name__ == "__main__":
     expr = DividedBy(Constant(1), X())
 
     print(expr.derivative()(1))
-    print(expr.derivative())
+    print(expr.derivative().simplify())
